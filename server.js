26c0403876a42fb04db6df3bba722862805ae4c5
Chillen <josh.jenkin@live.com>
2019-04-20 16:24:46 -0600

    relink db

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var S3_BUCKET = process.env.S3_BUCKET;
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
